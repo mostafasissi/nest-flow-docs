48033fb10062ea13def4d582170f9dc90488a8a4
Sissi Mostafa <101025147+mostafasissi@users.noreply.github.com>
2023-09-01 12:16:59 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 // .vitepress/config.js
 export default {
     // site-level options
+    base : 'nest-flow-docs' , 
     title: 'Nest flow',
 
     description: 'A nest js code generator using Yeoman',
